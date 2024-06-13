63cfbefa6559190da76620a3b0d15ddddc5680c7
joel magana <joelmagana15@gmail.com>
2024-06-13 00:04:53 -0700

    first

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,14 @@
-#Enable Holberton to login without errors
-
-#Increase the hard nofile limit
+# Increase the hard nofile limit for the holberton user
 exec { 'increase-hard-nofile-limit-for-user-Holberton':
-  command => "sed -i '/^holberton *hard.*nofile/s/4/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
+  command => "sed -i '/^holberton *hard.*nofile/s/[0-9]\\+/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+  unless  => "grep -E '^holberton *hard *nofile *50000' /etc/security/limits.conf",
 }
 
-
-#Increase the soft nofile limit
+# Increase the soft nofile limit for the holberton user
 exec { 'increase-soft-nofile-limit-for-user-Holberton':
-  command => "sed -i '/^holberton *soft/s/5/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
+  command => "sed -i '/^holberton *soft.*nofile/s/[0-9]\\+/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+  unless  => "grep -E '^holberton *soft *nofile *50000' /etc/security/limits.conf",
 }
+
